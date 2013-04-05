e428bf4bb17f52d7ae957ac6c9387bfbff242115
danielzzz <daniel@zelisko.net>
2013-04-05 12:15:25 +0200

    uncommented examples

--- example/example.js
+++ example/example.js
@@ -6,7 +6,7 @@ var host = '192.168.1.1',
 
 
 //------ check first available port between start and endPort on host ---------------
-/*console.log('checking ports');
+console.log('checking ports');
 portchecker.getAllOpen(startPort, endPort, host, function(openPorts, host) {
     openPorts.length && console.log('open ports on ' + host + ' between ' + startPort + ' and ' + endPort + ' are: ' + openPorts.join(', '));
     !openPorts.length && console.log('no open ports in given range on ' + host);
@@ -18,7 +18,7 @@ portchecker.getFirstAvailable(startPort, endPort, host, function(p, host) {
     } else {
         console.log('the first free port found on ' + host + ' between ' + startPort + ' and ' + endPort + ' is ' + p);
     }
-});*/
+});
 
 
 //---- check if the port is open ---------------------------------------------------
